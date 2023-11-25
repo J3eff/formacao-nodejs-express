413e317fe87c94e8ba9e8ac8dcc96acadf2edd5a
J3eff <jeffersonvideo125@gmail.com>
2023-11-25 16:21:13 -0300

    Criando registros com POST

--- express-mongo/src/app.js
+++ express-mongo/src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 const PORT = 3000;
 
 const app = express();
+app.use(express.json()); // Middleware
 
 const livros = [
     {
@@ -21,6 +22,11 @@ app.get('/', (req, res) => {
 
 app.get('/livros', (req, res) => {
     res.status(200).json(livros);
+});
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso');
 })
 
 export default app;
