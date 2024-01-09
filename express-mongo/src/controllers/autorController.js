b6fa4ba59d670585bd5e74bd68f0106f46e78a76
J3eff <jeffersonvideo125@gmail.com>
2024-01-09 02:20:08 -0300

    Tratando erros de busca por id

--- express-mongo/src/controllers/autorController.js
+++ express-mongo/src/controllers/autorController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { autor } from "../models/Autor.js";
 
 class AutorController {
@@ -14,16 +15,25 @@ class AutorController {
   static async listarAutorPorId(req, res) {
     try {
       const id = req.params.id;
+
       const autorEncontrado = await autor.findById(id);
-      res.status(200).json(autorEncontrado);
+
+      if (autorEncontrado !== null)
+        res.status(200).json(autorEncontrado);
+      else
+        res.status(404).json({ message: "Id do Autor não localizado." });
+
     } catch (error) {
-      res.status(500).json({ message: `${error.message} - falha na requisição do autor` });
+      if (error instanceof mongoose.Error.CastError)
+        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos." });
+      else
+        res.status(500).json({ message: `${error.message} - falha na requisição do autor` });
     }
   }
 
   static async cadastrarAutor(req, res) {
     try {
-      const novoAutor= await autor.create(req.body);
+      const novoAutor = await autor.create(req.body);
       res.status(201).json({ message: "Criado com sucesso", autor: novoAutor });
     } catch (error) {
       res.status(500).json({ message: `${error.message} - falha ao cadastrar do autor` });
@@ -39,7 +49,7 @@ class AutorController {
       res.status(500).json({ message: `${error.message} - falha na atualização` });
     }
   }
-    
+
   static async excluirAutor(req, res) {
     try {
       const id = req.params.id;
